0773bd5ba1802f5ea54bfc37880918e45f524f41
Lizz Thabet <lizzthabet@gmail.com>
2018-04-24 18:03:05 -0400

    build data schema and models, start sync call

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const router = require('./routes');
+const models = require('./models');
 
 
 app.set('view engine', 'html');
@@ -15,7 +16,16 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('server is running'));
+models.User.sync()
+	.then(() => {
+		console.log('User table created!');
+		return models.Page.sync();
+	})
+	.then(() => {
+		console.log('Page table created!');
+		app.listen(3000, () => console.log('server is running'));
+	})
+	.catch(console.error.bind(console));
 
 app.get('/', router);
 app.use(function (req, res) {
--- models/index.js
+++ models/index.js
@@ -1,2 +1,24 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
+
+const Page = db.define('page', {
+	title: Sequelize.STRING,
+	urlTitle: Sequelize.STRING,
+	content: Sequelize.TEXT,
+	status: Sequelize.ENUM('open', 'closed')
+})
+
+const User = db.define('user', {
+	name: Sequelize.STRING,
+	email: {
+		type: Sequelize.STRING,
+		validate: {
+			isEmail: true
+		}
+	}
+})
+
+module.export = {
+	Page,
+	User
+}
