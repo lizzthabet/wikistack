5fb3239a2fd3105c1aa19b38f9a66d2a27388f71
cofuente <cmarmar@gmail.com>
2018-04-25 15:36:54 -0400

    'added generateUrlTitle to models/index.js'

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const wikiRouter = express.Router();
+const models = require('../models')
+const Page = models.Page
+const User = models.User
+const express = require('express')
+const wikiRouter = express.Router()
 
 wikiRouter.get('/', (req, res) => {
-	res.redirect('/')
+  res.redirect('/')
 })
 
 wikiRouter.get('/add', (req, res) => {
-	res.render('addpage')
+  res.render('addpage')
 })
 
-wikiRouter.post('/', (req, res) => {
-	res.send('Hi you did it.')
+wikiRouter.post('/', (req, res, next) => {
+  //   res.json(req.body)
+  //   console.log(req.body)
+  const page = Page.build({
+    title: req.body.title,
+    content: req.body['page-content'],
+    status: req.body['page-status']
+    //   date: ???
+  })
+  page.save().then(res.redirect('/'))
 })
 
-module.exports = wikiRouter;
+module.exports = wikiRouter
